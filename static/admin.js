02dd4a27ece52efdb149c5ebcdd97e1766bbd8f9
hihivuiqua6-create <hihivuiqua6@gmail.com>
2026-06-13 15:44:13 +0700

    Delete static directory

--- static/admin.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('click', e => {
-  const a = e.target.closest('a[href*="approve"],a[href*="reject"],a[href*="clearlogs"]');
-  if(a && !confirm('Xác nhận thao tác này?')) e.preventDefault();
-});
